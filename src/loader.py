eeaeb1f2a18cc13e749087fd8aa507b089aecca5
Karimullin Arthur <KarimullinArthur@riseup.net>
2023-05-17 01:41:11 +0500

    Enable html parse mode

--- src/loader.py
+++ src/loader.py
@@ -9,7 +9,7 @@ from db.db import Database
 storage = RedisStorage2('localhost', 6379, db=5, pool_size=10,
                         prefix='my_fsm_key')
 
-bot = Bot(config.BOT_TOKEN)
+bot = Bot(config.BOT_TOKEN, parse_mode='HTML')
 dp = Dispatcher(bot, storage=storage)
 db = Database(f'dbname={config.PG_DATABASE} user={config.PG_USER}')
 scheduler = AsyncIOScheduler()
